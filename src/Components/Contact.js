5e9756d2505bdd79411e297468d1f7b07ffb9299
nagapandi <nagapandieee1998@gmail.com>
2023-09-13 10:15:29 +0530

    contact image added

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,28 +7,34 @@ import { FaInstagramSquare } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa';
 import { FaCloudDownloadAlt } from 'react-icons/fa';
 import { BiLogoGithub } from 'react-icons/bi';
-
+import Tooltip from '@mui/material/Tooltip';
 
 const Contact = () => {
     return (
         <div id="contact" class="contact">
             <div class="container">
-                <div class="contacts">
-                    <h1 className='pinkname' data-aos="flip-up" data-aos-duration="2000">Contact Me</h1>
-                    <p><FaEnvelope /> <a href="mailto:nagapandieee1998@gmail.com">nagapandieee1998@gmail.com</a></p>
-                    <p><FaPhoneSquareAlt /> <a href="tel:+91-6374601412">+91 6374601412</a></p>
-                    <div class="social-icons">
-                        <a href="https://www.facebook.com/nagapandi.nagapandi.7547/" target='blank'><FaFacebook /></a>
-                        <a href="https://twitter.com/nagapandi25" target='blank'><FaTwitterSquare /></a>
-                        <a href="https://www.instagram.com/_nagapandi" target='blank'><FaInstagramSquare /></a>
-                        <a href="https://www.linkedin.com/in/nagapandi-g-51a06b1b9" target='blank'><FaLinkedin /></a>
-                        <a href="https://github.com/nagapandi1998" target='blank'><BiLogoGithub /></a>
+                <div className='row'>
+                    <div className='contactimage contact-col-1'>
+                        <img src={require("./Assets/contact.jpg")} alt="" />
                     </div>
-                    <div data-aos="zoom-in" data-aos-offset="50" data-aos-duration="1000" class="cv">
-                        <a href="Nagapandi Resume.pdf" download="Nagapandi Resume.pdf"><i class="fa-regular fa-circle-down fa-bounce fa-lg"></i> <FaCloudDownloadAlt className='cvdownloadicon' />Download CV</a>
-
+                    <div class="contacts contact-col-2">
+                        <h1 className='pinkname' data-aos="flip-up" data-aos-duration="2000">Contact Me</h1>
+                        <p><FaEnvelope /> <a href="mailto:nagapandieee1998@gmail.com">nagapandieee1998@gmail.com</a></p>
+                        <p><FaPhoneSquareAlt /> <a href="tel:+91-6374601412">+91 6374601412</a></p>
+                        <div class="social-icons">
+                            <a href="https://www.facebook.com/nagapandi.nagapandi.7547/" target='blank'><FaFacebook /></a>
+                            <a href="https://twitter.com/nagapandi25" target='blank'><FaTwitterSquare /></a>
+                            <a href="https://www.instagram.com/_nagapandi" target='blank'><FaInstagramSquare /></a>
+                            <a href="https://www.linkedin.com/in/nagapandi-g-51a06b1b9" target='blank'><FaLinkedin /></a>
+                            <a href="https://github.com/nagapandi1998" target='blank'><BiLogoGithub /></a>
+                        </div>
+                        <div data-aos="zoom-in" data-aos-offset="50" data-aos-duration="1000" className="cv">
+                            <Tooltip title="Click to Download" arrow  placement="top">
+                                <a href="Nagapandi Resume.pdf" download="Nagapandi Resume.pdf">
+                                    <FaCloudDownloadAlt className='cvdownloadicon' />Download CV</a>
+                            </Tooltip>
+                        </div>
                     </div>
-
                 </div>
             </div>
         </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,8 +7,14 @@ const Home = () => {
       <div className='homescontainer'>
         <div className='row'>
           <div className='home-col-1'>
-            <h1 data-aos="fade-right" data-aos-duration="1800">Hi, I'm Nagapandi <span className='pinkname'>Gandhi</span></h1>
-            <h1 data-aos="fade-left" data-aos-duration="2000">ReactJS Developer</h1>
+            <h1 className='homeheader' data-aos="fade-right" data-aos-duration="1800">Hi, I'm Nagapandi <span className='pinkname'>Gandhi</span></h1>
+            <h1 className='homeheader' data-aos="fade-left" data-aos-duration="2000">ReactJS Developer</h1>
+            <div>
+              <h1 className='pinkname'>About</h1>
+              <p>To find a suitable position to challenge my web development skills to create scalable
+                web applications for diverse businesses. Trained in ReactJS, HTML, JS and basic web
+                development course.</p>
+            </div>
           </div>
           <div className='home-col-2'>
             <img data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000" src={require('./Assets/avatar.png')} alt="" />
